d77268a5fbfcbe7d7f1e7aa89d2a96caefea1007
Haris Gusic <harisgusic.dev@gmail.com>
2020-01-22 23:36:26 +0100

    Implement functions eeprom_read_xx

--- src/data.c
+++ src/data.c
@@ -1,1 +1,19 @@
 #include "data.h"
+
+inline char eeprom_read_byte(char address)
+{
+    EEADRL = address;
+    EECON1bits.RD = 1;
+    return EEDATL;
+}
+
+inline void eeprom_read_data(char address, unsigned char size, void *destination)
+{
+    char *dest = destination;
+    while (size--)
+    {
+        EEADRL = address++;
+        EECON1bits.RD = 1;
+        *dest++ = EEDATL;
+    }
+}
--- src/data.h
+++ src/data.h
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "definitions.h"
+
+inline char eeprom_read_byte(char address);
+
+/*
+ * Read a chunk of the specified size starting at 'address'
+ * and store it in the memory space pointed to by destination.
+ * 
+ * Note: The caller of the function must ensure that all
+ * parameters are valid and in-range.
+ */
+inline void eeprom_read_data(char address, unsigned char size, void *destination);
